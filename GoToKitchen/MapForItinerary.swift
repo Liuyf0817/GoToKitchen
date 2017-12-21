c55bb0614966a4e07813a32928e0e85551fe60d9
liuyanfeng <liuyf0817@gmail.com>
2017-12-21 12:16:11 +0800

    Add new file called MapForItinerary

---
+++ GoToKitchen/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GoToKitchen
+//
+//  Created by Liuyf0817 on 2017/12/21.
+//  Copyright © 2017年 Walter. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
